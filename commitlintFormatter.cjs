0bc25844459eec3746ed83c49ef772e6ef34a32d
Matthew Oluwajuwon <matthewoluwajuwon056@gmail.com>
2024-11-04 08:23:58 +0100

    feat: valid commit message

--- commitlintFormatter.cjs
+++ commitlintFormatter.cjs
@@ -1,8 +1,16 @@
 // commitlintFormatter.cjs
 module.exports = async (results) => {
   const { default: defaultFormatter } = await import('@commitlint/format'); // dynamic import
-  console.log('Running commitlintFormatter with results:', results);
+
   const formattedResults = defaultFormatter(results);
 
-  return `${formattedResults}\n\n🚨 Please use the following format:\n<type>(optional scope): <subject>\n\nExamples:\n- feat: add new payment gateway\n- fix(auth): correct login issue\n`;
+  if (results.valid) {
+    return `${formattedResults}\n\n🎉 Commit message is valid!`;
+  } else {
+    // Extract errors and warnings to create a more user-friendly error message
+    const errors = results.results.flatMap(result => result.errors.map(error => error.message)).join('\n');
+    const warnings = results.results.flatMap(result => result.warnings.map(warning => warning.message)).join('\n');
+
+    return `${formattedResults}\n\n🚨 Commit message is invalid!\n\nErrors:\n${errors}\n\nWarnings:\n${warnings}\n\nPlease use the following format:\n<type>(optional scope): <subject>\n\nExamples:\n- feat: add new payment gateway\n- fix(auth): correct login issue\n`;
+  }
 };
